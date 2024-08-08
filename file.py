b9d9a51d7edd7f6c8184b8b8eb6ce80924011107
Okwuosa Ikechukwu <102191989+Iyke2259@users.noreply.github.com>
2024-08-08 15:50:32 +0100

    Update file.py

--- file.py
+++ file.py
@@ -5,19 +5,20 @@ import numpy as np
 import pickle 
 import streamlit as st 
 
+
+pickle_in_oil = open('model_oil.pkl', 'rb')
+model_oil = pickle.load(pickle_in_oil)
+
 pickle_in_gas = open('model_gas.pkl', 'rb')
 model_gas = pickle.load(pickle_in_gas)
 
 pickle_in_water = open('model_water.pkl', 'rb')
 model_water = pickle.load(pickle_in_water)
 
-pickle_in_oil = open('model_oil.pkl', 'rb')
-model_oil = pickle.load(pickle_in_oil)
-
-
-
 st.set_page_config(page_title="Well Forecast App")
-def predictions(Date,Downhole_Pressure,Downhole_Temperature,Average_Tubing_Pressure,Annulus_Pressure,Average_Wellhead_Pressure,Choke_Size,WellBore_Name):
+def predictions(Date,Downhole_Pressure,Downhole_Temperature,
+                Average_Tubing_Pressure,Annulus_Pressure,
+                Average_Wellhead_Pressure,Choke_Size,WellBore_Name):
     
     # Preprocessing User input - Date
     Date = pd.to_datetime(Date)
@@ -30,62 +31,54 @@ def predictions(Date,Downhole_Pressure,Downhole_Temperature,Average_Tubing_Press
     # Preprocessing User input - Categorical Encoding
 
     if WellBore_Name == "001_F_12":
-        WellBore_Name_001_F_1_C = 0
-        WellBore_Name_001_F_11 = 0
-        WellBore_Name_001_F_12 = 1 
-        WellBore_Name_001_F_14= 0
-        WellBore_Name_001_F_15_D =0
+        bin_list = [0,0,1,0,0]    
     elif WellBore_Name == '001_F_14':
-        WellBore_Name_001_F_1_C = 0
-        WellBore_Name_001_F_11 = 0
-        WellBore_Name_001_F_12 = 0 
-        WellBore_Name_001_F_14= 1
-        WellBore_Name_001_F_15_D =0
+        bin_list = [0,0,0,1,0]
     elif WellBore_Name =='001_F_11':
-        WellBore_Name_001_F_1_C = 0
-        WellBore_Name_001_F_11 = 1
-        WellBore_Name_001_F_12 = 0
-        WellBore_Name_001_F_14= 0
-        WellBore_Name_001_F_15_D =0
+        bin_list = [0,1,0,0,0]
     elif WellBore_Name == '001_F_15 D':
-        WellBore_Name_001_F_1_C = 0
-        WellBore_Name_001_F_11 = 0
-        WellBore_Name_001_F_12 = 0 
-        WellBore_Name_001_F_14= 0
-        WellBore_Name_001_F_15_D =1
+        bin_list = [0,0,0,0,1]
     elif WellBore_Name == '001_F_1 C':
-        WellBore_Name_001_F_1_C = 1
-        WellBore_Name_001_F_11 = 0
-        WellBore_Name_001_F_12 = 1 
-        WellBore_Name_001_F_14= 0
-        WellBore_Name_001_F_15_D =0
+        bin_list = [1,0,0,0,0]
     # Engineered Features
     pressure_differential = Downhole_Pressure - Average_Wellhead_Pressure
     pressure_differential_annulus = Average_Tubing_Pressure - Annulus_Pressure
     P_T_Ratio = Downhole_Pressure/Downhole_Temperature
-    # Predictions 
-    oil_predictions = model_oil.predict([[Downhole_Pressure, Downhole_Temperature,Average_Tubing_Pressure, 
-                                          Annulus_Pressure ,Average_Wellhead_Pressure, Choke_Size, 
-                                          WellBore_Name_001_F_1_C, WellBore_Name_001_F_11, WellBore_Name_001_F_12,
-                                          WellBore_Name_001_F_14, WellBore_Name_001_F_15_D, day_of_week, year, 
-                                          yearly_quarter,day_of_year, month_of_year, pressure_differential,
-                                          pressure_differential_annulus, P_T_Ratio
-                                         ]])
+    
+    # Oil Production Predictions 
+    oil_user_input = [Downhole_Pressure, Downhole_Temperature,Average_Tubing_Pressure, 
+                      Annulus_Pressure ,Average_Wellhead_Pressure, Choke_Size, 
+                      day_of_week, year,yearly_quarter,day_of_year, month_of_year, 
+                      pressure_differential,pressure_differential_annulus, P_T_Ratio]
+    i = 0
+    while i < len(bin_list):
+        oil_user_input.insert(6+i, bin_list[i])
+        i = i + 1
+    oil_predictions = model_oil.predict([oil_user_input])                                 
     oil_predictions = max(0, oil_predictions)
-        
-    gas_predictions = model_gas.predict([[Downhole_Pressure, Downhole_Temperature,Average_Tubing_Pressure, 
-                                          Annulus_Pressure ,Average_Wellhead_Pressure, Choke_Size, 
-                                          WellBore_Name_001_F_1_C, WellBore_Name_001_F_11, WellBore_Name_001_F_12,
-                                          WellBore_Name_001_F_14, WellBore_Name_001_F_15_D, day_of_week, year, 
-                                          yearly_quarter,day_of_year, month_of_year, pressure_differential,
-                                          pressure_differential_annulus]])
+
+    # Gas Production Predictions
+    gas_user_input = [Downhole_Pressure, Downhole_Temperature,Average_Tubing_Pressure, 
+                      Annulus_Pressure ,Average_Wellhead_Pressure, Choke_Size, 
+                      day_of_week, year, yearly_quarter,day_of_year, month_of_year,
+                      pressure_differential, pressure_differential_annulus] 
+    i = 0
+    while i < len(bin_list):
+        gas_user_input.insert(6+i, bin_list[i])
+        i = i + 1
+    gas_predictions = model_gas.predict([gas_user_input])
     gas_predictions = max(0, gas_predictions)
-       
-    water_predictions = model_water.predict([[Downhole_Pressure, Downhole_Temperature, Average_Tubing_Pressure, 
-                                              Annulus_Pressure ,Average_Wellhead_Pressure, Choke_Size, 
-                                              WellBore_Name_001_F_1_C, WellBore_Name_001_F_11, WellBore_Name_001_F_12,
-                                              WellBore_Name_001_F_14, WellBore_Name_001_F_15_D, day_of_week, year, 
-                                              pressure_differential, pressure_differential_annulus]])
+
+    #Water Production Predictions
+    water_user_input = [Downhole_Pressure, Downhole_Temperature, Average_Tubing_Pressure, 
+                      Annulus_Pressure ,Average_Wellhead_Pressure, Choke_Size, 
+                      day_of_week, year, pressure_differential, pressure_differential_annulus
+                      ]
+    i = 0
+    while i < len(bin_list):
+        water_user_input.insert(6+i, bin_list[i])
+        i = i + 1
+    water_predictions = model_water.predict([water_user_input])
     water_predictions = max(0, water_predictions)
     
     return oil_predictions, gas_predictions, water_predictions
@@ -94,12 +87,10 @@ def predictions(Date,Downhole_Pressure,Downhole_Temperature,Average_Tubing_Press
     
 def main():
     st.title("Well Production Forecasting using Machine Learning")
-    #st.sidebar.header(" Web App")
-    st.write("This web app utilizes machine learning techniques to accurately forecast the production rates for oil, gas, and water in a drilling well. The ML model was trained on well production data from a conceptual oil field operated between 2008 and 2015.")
-    st.write("<i>Fill in the appropriate parameters below to generate a well production estimate:</i>",unsafe_allow_html=True)
+    st.write("This web app utilizes machine learning techniques to accurately forecast production rates of oil, gas, and water from a production well. The model was trained on data from a conceptual oil field operated between 2008 and 2015.")
+    st.write("<i>Fill in the parameters below to generate a well production estimate:</i>",unsafe_allow_html=True)
     
-      
-    Date = st.date_input("Forecast Date",help="ML model was trained on production data between 2008 and 2015")
+    Date = st.date_input("Forecast Date")
     WellBore_Name = st.selectbox("Wellbore Name", ['001_F_12', '001_F_14', '001_F_11', '001_F_15 D', '001_F_1 C'],
                                  help="Wellbore IDs as provided in training data")
     Downhole_Temperature = st.slider('Downhole Temperature(kelvin)', min_value=273, max_value=400)
@@ -109,15 +100,11 @@ def main():
     Annulus_Pressure = st.slider('Annulus Pressure(psi)', min_value=0, max_value=450)
     Average_Wellhead_Pressure = st.slider('Average Wellhead Pressure(psi)', min_value=0, max_value=2000)
     Choke_Size = st.slider('Choke Size(/64)', min_value=0, max_value=100)
-
-    # add element on the right side
-    # User Input
-   
     
     if st.button("Production Forecast"):
-        #st.progress(100,text= "Operation in progress. Please wait.")
-        oil_prediction, gas_prediction, water_prediction  = predictions(Date,Downhole_Pressure,Downhole_Temperature,Average_Tubing_Pressure,
-                    Annulus_Pressure,Average_Wellhead_Pressure,Choke_Size,WellBore_Name)
+        oil_prediction,gas_prediction,water_prediction=predictions(Date,Downhole_Pressure,Downhole_Temperature,
+                                                                Average_Tubing_Pressure,Annulus_Pressure,
+                                                                Average_Wellhead_Pressure,Choke_Size,WellBore_Name)
     
         st.success(f"Oil Production Forecast: {"{:,}".format(int(oil_prediction))} stb/day")
         st.success(f"Gas Production Forecast: {"{:,}".format(int(gas_prediction))} scf/day")
