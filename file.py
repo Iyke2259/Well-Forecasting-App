adc7c6396eed5886ddcc68b2c4b70d40146c30c1
Okwuosa Ikechukwu <102191989+Iyke2259@users.noreply.github.com>
2024-08-12 19:38:36 +0100

    Update file.py

--- file.py
+++ file.py
@@ -4,18 +4,23 @@ import pandas as pd
 import numpy as np 
 import pickle 
 import streamlit as st 
+import sklearn
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
-
+# Loading saved models
 pickle_in_oil = open('model_oil.pkl', 'rb')
 model_oil = pickle.load(pickle_in_oil)
 
 pickle_in_gas = open('model_gas.pkl', 'rb')
 model_gas = pickle.load(pickle_in_gas)
+pkl_scaler_gas = open('scaler_gas.pkl', 'rb')
+scaler_gas = pickle.load(pkl_scaler_gas)
 
 pickle_in_water = open('model_water.pkl', 'rb')
 model_water = pickle.load(pickle_in_water)
 
 st.set_page_config(page_title="Well Forecast App")
+
 def predictions(Date,Downhole_Pressure,Downhole_Temperature,
                 Average_Tubing_Pressure,Annulus_Pressure,
                 Average_Wellhead_Pressure,Choke_Size,WellBore_Name):
@@ -61,18 +66,20 @@ def predictions(Date,Downhole_Pressure,Downhole_Temperature,
     gas_user_input = [Downhole_Pressure, Downhole_Temperature,Average_Tubing_Pressure, 
                       Annulus_Pressure ,Average_Wellhead_Pressure, Choke_Size, 
                       day_of_week, year, yearly_quarter,day_of_year, month_of_year,
-                      pressure_differential, pressure_differential_annulus] 
+                      pressure_differential, pressure_differential_annulus, P_T_Ratio] 
     i = 0
     while i < len(bin_list):
         gas_user_input.insert(6+i, bin_list[i])
         i = i + 1
-    gas_predictions = model_gas.predict([gas_user_input])
+    gas_user_input = scaler_gas.transform(np.array(gas_user_input).reshape(1,-1))
+    gas_predictions = model_gas.predict(gas_user_input)
     gas_predictions = max(0, gas_predictions)
 
     #Water Production Predictions
     water_user_input = [Downhole_Pressure, Downhole_Temperature, Average_Tubing_Pressure, 
                       Annulus_Pressure ,Average_Wellhead_Pressure, Choke_Size, 
-                      day_of_week, year, pressure_differential, pressure_differential_annulus
+                      day_of_week, year, yearly_quarter,day_of_year, month_of_year,
+                     pressure_differential, pressure_differential_annulus, P_T_Ratio
                       ]
     i = 0
     while i < len(bin_list):
